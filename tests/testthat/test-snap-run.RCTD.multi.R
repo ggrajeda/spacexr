dcfbe23dab77c3507198c911b643c3245efb377a
Robert Shear <rshear@gmail.com>
2024-08-28 16:46:34 -0400

    Add multi-core test to test-snap-runRCTD.multi

--- tests/testthat/test-snap-run.RCTD.multi.R
+++ tests/testthat/test-snap-run.RCTD.multi.R
@@ -6,13 +6,30 @@ test_that("run.RCTD.multi", {
                                   cells_per_type = 60,
                                   nGenes = 500,
                                   seed = 886))
-  rctd <- create.RCTD(mat$puck[[1]], mat$reference, max_cores = 1)
 
-  # Act/Assert
+  # Act
+  rctd <- create.RCTD(mat$puck[[1]], mat$reference, max_cores = 1)
+  rctd_multi <- create.RCTD(mat$puck[[1]], mat$reference, max_cores = 2)
+
+  # list of externally relevant results
+  result_list <- \(u) {
+    list(cell_type_info = u@cell_type_info$renorm,
+         de_results = u@de_results,
+         results = u@results)
+  }
+  raw_result <- run.RCTD(rctd, doublet_mode = 'multi')
+  result <- result_list(raw_result)
+
+  raw_result_multi <- run.RCTD(rctd_multi, doublet_mode = 'multi')
+  result_multi <- result_list(raw_result)
+
+
+  # Assert
   expect_snapshot({
-    result <- run.RCTD(rctd, doublet_mode = 'multi')
-    list(cell_type_info = result@cell_type_info$renorm,
-         de_results = result@de_results,
-         results = result@results)
-    })
+    result
+    u <- result$results$results_df
+    print(cf <- table(substr(rownames(u),1,3), u$first_type))
+  })
+
+  expect_equal(result_multi, result)
 })
