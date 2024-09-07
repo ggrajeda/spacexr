64613b4e64e1e03610b181d4602ae8dbfe432fcc
Robert D Shear <rshear@gmail.com>
2024-09-07 17:58:33 -0400

    revised geometry ready for test.

--- tests/testthat/helper-test_data_generators.R
+++ tests/testthat/helper-test_data_generators.R
@@ -2,18 +2,6 @@ library(stringr, quietly = TRUE)
 library(splatter, quietly = TRUE)
 library(scater, quietly = TRUE)
 
-# convert a UTF-8 string to base 4 (bytewise bigendian),
-#   where the digits are A,C,G,T
-# Puropose: To create pseduo bar codes
-# example: char2atcg("A") => "CAAC"
-# "A" asici 65 (base 10) =>  01 10 00 10 => 2,0,2,1 => GAGC
-char2atcg <- Vectorize(\(s) {
-  x <- utf8ToInt(s)
-  paste0(c("A", "C", "G", "T")[t(sapply(0:3,
-        function(u) bitwAnd(bitwShiftR(x, u*2), 3))+1)], collapse = "")})
-
-#
-
 #' Create a synthetic dataset of RNA-seq like counts
 #'
 #' Simulates asngle-cell RNA-seq experiment and attaches 2-d coordinates to each observation.
@@ -27,6 +15,7 @@ char2atcg <- Vectorize(\(s) {
 #' @param de.prob - The probability that a given gene will be differentially expressed.
 #' Either a single percentage, or a vector of percentages, one foe each of the cell types. (Default: linearly increasing probabilities from 0.3 to 0.4)
 #' @param nGenes - The number of genes.
+#' TODO REMVOE DISK_RADIUS
 #' @param disk_radius - The radius of the disk that contains the individual observations for the cell type.
 #' @param seed - A random seed. This assures that the same simulated SCE results will be returned each time that this function is called.
 #' It does not use or alter the random seed value of the calling function.
@@ -47,30 +36,6 @@ synthetic_se <- function(n_celltypes = 3,
       verbose = FALSE
     )
     colData(se)[, "Group"] <- as.factor(str_replace(colData(se)$Group, "Group", "ct"))
-    # TODO DELETE WHEN revision is working
-#
-#     # Create barcode as CellType_UMI
-#     # where CellType is from Group
-#     colnames(se) <- paste(colData(se)$Group, char2atcg(colData(se)$Cell), sep = "_")
-#
-#     # map the cells (columns) to slide xy-space
-#     cartesian2polar <- function(cart) with(cart,data.frame(rho = sqrt(x^2 + y^2),
-#                                                            theta = atan2(y, x)))
-#     polar2cartesian <- function(polar) with(polar, data.frame(x = rho * cos(theta),
-#                                                               y = rho * sin(theta)))
-#     # the groups are evenly spaced along the circumference of a circle
-#     theta <- 2*pi * 0:(n_celltypes - 1) /n_celltypes
-#     centroids <- polar2cartesian(data.frame(rho = 1, theta = theta))
-#     runif(total_cells, 0, 2  * pi)
-#
-#     # the cells (columns) are distributed in a smaller disk centered at each group
-#     cd <- colData(se)
-#     location <- polar2cartesian(data.frame(rho = sqrt(runif(total_cells, 0, 1)) * disk_radius,
-#                                            theta = runif(total_cells, 0, 2  * pi)))
-#     offset <- centroids[as.integer(colData(se)$Group),]
-#     location <- location + offset
-#     # the (x,y) coordinates for eacch cell are now part of colData
-#     colData(se) <- cbind(cd, location)
   })
   return(se)
 }
@@ -82,14 +47,10 @@ synthetic_se <- function(n_celltypes = 3,
 #' The remaining columns will be used as the experimental observations.
 #' @param replicates The number of experimental replicates
 #'
-#' @return list(reference, list(s_regions), sce)
+#' @return list(reference, list(s_regions), list(sce)) # TODO describe resutls
 #'
 sce_to_rctd <- function(sce, prop.ref = 0.5, replicates = 1,
-                        region_spec = list(
-                          ct1 = list(location=c(x=1,y=1), size=c(x=10,y=8), mixture=c(ct1=1)),
-                          ct2 = list(location=c(x=1,y=10), size=c(x=10,y=8), mixture=c(ct2=1)),
-                          ct3 = list(location=c(x=1,y=20), size=c(x=10,y=8), mixture=c(ct3=1))
-                        )) {
+                        puck_pattern = config_bars_for_cell_types) {
   # create Reference object
   if (prop.ref == 0) {
     # If no Reference object is to be built
@@ -102,49 +63,47 @@ sce_to_rctd <- function(sce, prop.ref = 0.5, replicates = 1,
     names(cell_types) <- colnames(refSE)
     reference <- Reference(counts =assay(refSE, "counts"),
                            cell_types = cell_types)
-  }
-  # TODO need enough dots per region
-  sce <- sce[, split:ncol(sce)]
-  r2d <-regions_to_dots(region_spec)
-  # assign bar codes (proxy for dot location) to colnames
-  v <- split(seq(ncol(sce)), colData(sce)$Group)
-  for (i in seq_along(v)) {
-    colnames(sce)[v[[i]]] <- sample(rownames(r2d[[i]]), length(v[[i]]))
+    sce <- sce[, (split+1):ncol(sce)]
   }
 
-  r2c_reduced <- Reduce(\(x,y) rbind(x, y), r2d)
-  colData(sce) <- cbind(colData(sce), r2c_reduced[colnames(sce),])
 
-  # now colData has the bar code and x,y coordinates
-
-  # The boundaries for for the column partition
-  u <- round(seq(from = 1, to = ncol(sce)+ 1, length.out = replicates + 1))
-  # each row of of u is a replicate and the two columns are the corresponding
-  # limits of the columns to be returned from sce
-  u <- matrix(c(u[1:(replicates)], u[2:(replicates + 1)] - 1),
-              nrow = replicates, byrow = FALSE)
-  # check to be sure that there there is at least one column in each replicate
-  if ( min(u[,2] - u[,1]) <1) {
-    stop("function sce_to_rctd: Not enough sce columns for the number of replicates")
-  }
+  # Assign replicate numbers (1..n) to each column, distirubiton the
+  # cell types evenly between the groups
+  # This is policy is for test data and not a simulation of real replicates
+  z <- split(seq(ncol(sce)), colData(sce)$Group)
+  replicate <- unlist(sapply(seq_along(z), \(i) rep(1:replicates, length.out = length(z[[i]]))))
+  colData(sce) <- cbind(colData(sce)[unlist(z), ], data.frame(replicate))
 
   # create s_regions
 
-  s_regions <- apply(u, 1, \(x) {
-    s <- sce[,x[1]:x[2]]
+  s_regions <- lapply(split(1:ncol(sce), colData(sce)$replicate), \(sindex) {
+    s <- sce[, sindex]
+    # assign bar codes (proxy for dot location) to colnames
+    v <- split(seq(ncol(s)), colData(s)$Group)
+    cell_type_count <- sapply(v, length)
+    region_spec <- puck_pattern(cell_type_count)
+    r2d <-regions_to_dots(region_spec)
+    for (i in seq_along(v)) {
+      colnames(s)[v[[i]]] <- sample(rownames(r2d[[i]]), length(v[[i]]))
+    }
+
+    r2c_reduced <- Reduce(\(x,y) rbind(x, y), r2d)
+    colData(s) <- cbind(colData(s), r2c_reduced[colnames(s),])
+
     v <- list(counts = assay(s, "counts"),
               coords = as.data.frame(colData(s)[,c("x", "y")]))
     v$nUMI <- colSums(v$counts)
     s_region <- SpatialRNA(v$coords, v$counts)
+
     s_regions <- list(s_region) # TODO plug for missing replicate loop
     v <- list(counts = assay(s, "counts"),
               coords = as.data.frame(colData(s)[,c("x", "y")]))
     v$nUMI <- colSums(v$counts)
     puck <- SpatialRNA(v$coords, v$counts)
+    list(puck, s)
   })
-  names(s_regions) <- paste("rpl", seq_along(s_regions), sep = "")
-
-  list(reference = reference, s_regions = s_regions, se = sce)
+  list(reference = reference, s_regions = lapply(s_regions, `[[`, 1),
+       se = lapply(s_regions, `[[`, 2))
 }
 
 # spatial data
@@ -157,35 +116,29 @@ sce_to_rctd <- function(sce, prop.ref = 0.5, replicates = 1,
 # name = list(location, size, mixture)
 
 config_bars_for_cell_types <- \(cell_type_table, region_width = 10, margin = 2) {
-cell_type_table<-foo
-region_width = 10
-margin = 2
-  df <- data.frame(name=cell_type_table, stringsAsFactors = FALSE)
-  colnames(df) <- c("cell_type", "pixel_count")
+  df <- data.frame(cell_type=names(cell_type_table),
+                   pixel_count=as.integer(cell_type_table),
+                   stringsAsFactors = FALSE)
   df$size.y <- ceiling(df$pixel_count / region_width)
   df$loc.x <- 1
   df$loc.y <- c(1, cumsum(df$size.y + margin)[-nrow(df)])
   df$size.x <- region_width
   df$mixture <- 1
   names(df$mixture) <- df$cell_tye
-  df
 
-  apply(df[1,], 1, \(u) list(location=c(u[["loc.x"]]), u[["loc.y"]]))
+  result <- output_list <- lapply(1:nrow(df), function(i) {
+    cell <- df$cell_type[i]
+    list(
+      location = c(x = df$loc.x[i], y = df$loc.y[i]),
+      size = c(x = df$size.x[i], y = df$size.y[i]),
+      mixture = setNames(c(df$mixture[i]), cell)
+    )
+  })
 
-
-  loc <- c(x=1,y=1)
-  result <- mapply(\(ct, n) {
-    height <- ceiling(n / region_width)
-    m <- 1
-    names(m) <- ct
-    u <- list(location=loc, size=loc(region_width, height), mixture=m)
-    names(u) <- ct
-    loc <- loc + c(0, height + margin)
-    u
-  }, names(cell_type_table), cell_type_table)
-  # list(
-  #   ct1 = list(location=c(x=1,y=1), size=c(x=10,y=8), mixture=c(ct1=1)),
+  names(result) <- df$cell_type
+  result
 }
+
 
 # convert a UTF-8 string to base 4 (bytewise bigendian),
 #   where the digits are A,C,G,T
@@ -195,7 +148,7 @@ margin = 2
 char2atcg <- Vectorize(\(s) {
   x <- utf8ToInt(s)
   paste0(c("A", "C", "G", "T")[t(sapply(0:3,
-                                        function(u) bitwAnd(bitwShiftR(x, u*2), 3))+1)], collapse = "")})
+             function(u) bitwAnd(bitwShiftR(x, u*2), 3))+1)], collapse = "")})
 
 # enumerate the x,y positions (dots or "pixels") that are contained in each region.
 regions_to_dots <- \(config) {
