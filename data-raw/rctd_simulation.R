328c05578fb274415b1a33ee703caf0689e3d656
ggrajeda <gabriel.grajeda@gmail.com>
2025-01-14 15:22:39 -0500

    Create simulated dataset for examples, unit tests, and vignettes

--- data-raw/rctd_simulation.R
+++ data-raw/rctd_simulation.R
@@ -3,7 +3,7 @@
 # differentially expressed with probability prob_de.
 create_expression_profile <- function(num_genes, num_cell_types, prob_de) {
   # Generate a base expression profile for all cell types.
-  base_expr_vec <- round(runif(num_genes, min = 0, max = 5), digits = 2)
+  base_expr_vec <- round(runif(num_genes, min = 0, max = 5), digits = 1)
   expr_profile <- replicate(num_cell_types, base_expr_vec)
 
   # Introduce differential expression for a random set of genes.
@@ -19,14 +19,14 @@ create_expression_profile <- function(num_genes, num_cell_types, prob_de) {
 # one mixture.
 pure_mixtures <- function(num_cell_types, num_samples) {
   mixtures <- lapply(seq_len(num_cell_types), function(i) {
-    list(n = num_samples, prop = as.numeric(seq_len(num_cell_types) == i))
+    list(prop = as.numeric(seq_len(num_cell_types) == i), n = num_samples)
   })
   names(mixtures) <- paste0("ct", seq_len(num_cell_types))
   mixtures
 }
 
 # Generates a gene expression matrix for a cell type mixture.
-# See simulate_rctd_object for details on specifying the mixture.
+# See simulate_spatial_transcriptomics for details on specifying the mixture.
 create_mixed_counts <- function(expression_profile, mixture) {
   prop <- mixture$prop
   if (sum(prop) != 1) {
@@ -50,7 +50,7 @@ create_mixed_counts <- function(expression_profile, mixture) {
 # Generates a gene expression matrix for a list of cell type mixtures. This is
 # returned as a SummarizedExperiment, with column data specifying the mixture
 # type (mixture_type) and the instance of the mixture type (sample_num).
-# See simulate_rctd_object for details on specifying the mixtures.
+# See simulate_spatial_transcriptomics for details on specifying the mixtures.
 simulate_counts <- function(expression_profile, mixtures) {
   simulated_counts <- do.call(cbind, lapply(mixtures, function(m) {
     create_mixed_counts(expression_profile, m)
@@ -83,13 +83,14 @@ simulate_reference_object <- function(expression_profile, num_samples) {
 }
 
 # Generates a simulated SpatialRNA object from the mean expression profile of
-# every cell type and a list of mixtures. Each mixture is given a distinct
-# x-coordinate.
+# every cell type and a list of mixtures.
 simulate_spatial_rna_object <- function(expression_profile, test_mixtures) {
   test_data <- simulate_counts(expression_profile, test_mixtures)
+  sample_num <- colData(test_data)$sample_num - 1
+  print(sample_num)
   coords <- data.frame(
-    x = as.integer(colData(test_data)$mixture_type),
-    y = colData(test_data)$sample_num
+    x = as.integer(colData(test_data)$mixture_type) + (sample_num %% 5) / 6,
+    y = (sample_num %/% 5) / 5
   )
   rownames(coords) <- colnames(test_data)
 
@@ -98,48 +99,39 @@ simulate_spatial_rna_object <- function(expression_profile, test_mixtures) {
 
 #' Generate Simulated Spatial Transcriptomics Data
 #'
-#' @param num_genes Number of genes. (Default 5000)
-#' @param num_cell_types Number of cell types. (Default 3)
+#' @param num_genes Number of genes.
+#' @param num_cell_types Number of cell types.
 #' @param num_reference_samples Number of samples of each cell type to include
-#'   in the Reference object. (Default 100)
+#'   in the Reference object.
 #' @param test_mixtures Mixtures of cell types used to create the SpatialRNA
 #'   object, represented as a named list of named lists.
 #'
 #'   Each inner list should have two elements:
 #'   \itemize{
-#'   \item{\code{n}: }{Number of samples of the mixture to create}
 #'   \item{\code{prop}: }{Proportion of the cell types comprising the mixture,
 #'   represented by a vector whose length is equal to the number of cell types
 #'   and whose entries sum to 1.}
 #'   }
-#'   (Default is 25 samples of a pure mixture of each cell type)
+#'   \item{\code{n}: }{Number of samples of the mixture to create}
+#'   Default is 25 samples of a pure mixture of each cell type
 #'
-#'   Example: \code{list(mx1=list(n=10, prop=c(0.8, 0, 0.2)))}.
+#'   Example: \code{list(mx1 = list(prop = c(0.8, 0, 0.2), n = 10))}.
 #'
 #'   This specifies 10 samples of mixture \code{mx1}, consisting 80\% of cell
 #'   type 1 and 20\% of cell type 3.
 #' @param prob_de Probability that a gene will be differentially expressed.
-#'   (Default 0.1)
 #'
-#' @return A named list containing:
-#'   \itemize{
-#'   \item{\code{rctd}:}{ RCTD object constructed with a Reference object and a
-#'   SpatialRNA object as specified above.}
-#'   \item{\code{true_proportions}: }{Matrix where the \emph{n}th column specifies
-#'   the true proportion of cell type \emph{n} in the SpatialRNA samples.}
-#'   }
-#'
-#' @export
-#' @examples
-#' rctd_obj <- simulate_rctd_object()
-simulate_rctd_object <- function(num_genes = 5000,
-                                 num_cell_types = 3,
-                                 num_reference_samples = 100,
-                                 test_mixtures = pure_mixtures(
-                                   num_cell_types,
-                                   25
-                                 ),
-                                 prob_de = 0.1) {
+#' @return A named list containing the data for a simulated Reference object,
+#'  a simulated SpatialRNA object, and the true labels for the SpatialRNA
+#'  samples.
+simulate_spatial_transcriptomics <- function(num_genes = 750,
+                                             num_cell_types = 3,
+                                             num_reference_samples = 25,
+                                             test_mixtures = pure_mixtures(
+                                               num_cell_types,
+                                               15
+                                             ),
+                                             prob_de = 0.95) {
   expression_profile <- create_expression_profile(
     num_genes, num_cell_types, prob_de
   )
@@ -147,11 +139,24 @@ simulate_rctd_object <- function(num_genes = 5000,
     expression_profile, num_reference_samples
   )
   spatial_rna <- simulate_spatial_rna_object(expression_profile, test_mixtures)
-  rctd_object <- create.RCTD(spatial_rna, reference)
 
   true_proportions <- do.call(rbind, lapply(test_mixtures, function(m) {
     t(replicate(m$n, m$prop))
   }))
 
-  list(rctd = rctd_object, true_proportions = true_proportions)
+  list(
+    reference_counts = reference@counts,
+    reference_cell_types = reference@cell_types,
+    spatial_rna_coords = spatial_rna@coords,
+    spatial_rna_counts = spatial_rna@counts,
+    true_proportions = true_proportions
+  )
 }
+
+set.seed(123456789)
+rctd_simulation <- simulate_spatial_transcriptomics(test_mixtures=list(
+  mx1 = list(prop = c(0.5, 0.5, 0.0), n = 25),
+  mx2 = list(prop = c(0.5, 0.0, 0.5), n = 25),
+  mx3 = list(prop = c(0.2, 0.4, 0.4), n = 25)
+))
+usethis::use_data(rctd_simulation, overwrite = TRUE)
