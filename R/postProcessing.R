d5dfaf56cc61fb3967a7c62dfd139a6eaece5472
ggrajeda <gabriel.grajeda@gmail.com>
2025-02-10 20:23:08 -0500

    Add functions that return SummarizedExperiment from RCTD results

--- R/postProcessing.R
+++ R/postProcessing.R
@@ -160,3 +160,178 @@ import_weights <- function(myRCTD, weights) {
 normalize_weights <- function(weights) {
     return(sweep(weights, 1, rowSums(weights), "/"))
 }
+
+rctd_metadata <- function(RCTD) {
+    metadata <- list(
+        spatial_rna = RCTD@spatialRNA,
+        original_spatial_rna = RCTD@originalSpatialRNA,
+        reference = RCTD@reference,
+        config = RCTD@config,
+        cell_type_info = RCTD@cell_type_info,
+        internal_vars = RCTD@internal_vars
+    )
+    return(metadata)
+}
+
+#' Converts a list of results to a SummarizedExperiment
+#' 
+#' The SummarizedExperiment contains an assay with the cell type weights.
+#' Additional information (e.g., x and y coords) are stored in the row data,
+#' which will contain the entries in the results list specified by the *_cols
+#' arguments.
+#' 
+#' @param RCTD  RCTD object
+#' @param results List of results (with named entries) for each pixel
+#' @param weights_col Name of list entry containing the cell type weights
+#' @param character_cols Names of list entries containing a character(1)
+#' @param logical_cols Names of list entries containing a logical(1)
+#' @param numeric_cols Names of list entries containing a numeric(1)
+#' @param list_cols Names of list entries containing a list
+#'
+#' @importFrom SummarizedExperiment SummarizedExperiment
+#' @keywords internal
+create_se_from_columns <- function(
+    RCTD,
+    results,
+    weights_col = "all_weights",
+    character_cols = c(), 
+    logical_cols = c(), 
+    numeric_cols = c(), 
+    list_cols = c()
+) {
+    cell_type_names <- RCTD@cell_type_info$renorm[[2]]
+    weights <- vapply(
+        results,
+        function(r) r[[weights_col]],
+        numeric(length(cell_type_names))
+    )
+    weights <- Matrix(t(weights))
+
+    coords <- RCTD@spatialRNA@coords[, c("x", "y")]
+    row_data <- data.frame(coords)
+    for (cc in character_cols) {
+        get_character <- function(r) as.character(r[[cc]])
+        row_data[cc] <- vapply(results, get_character, character(1))
+    }
+    for (lc in list_cols) {
+        row_data[[lc]] <- lapply(results, function(r) r[[lc]])
+    }
+    for (lc in logical_cols) {
+        row_data[lc] <- vapply(results, function(r) r[[lc]], logical(1))
+    }
+    for (nc in numeric_cols) {
+        row_data[nc] <- vapply(results, function(r) r[[nc]], numeric(1))
+    }
+
+    metadata <- rctd_metadata(RCTD)
+
+    se <- SummarizedExperiment(
+        assays = list(weights = weights),
+        rowData = row_data,
+        metadata = metadata
+    )
+    return(se)
+}
+
+#' Converts the results of \code{decompose_batch} to a SummarizedExperiment
+#' 
+#' @param RCTD RCTD object
+#' @param results \code{decompose_batch} results
+#'
+#' @importFrom SummarizedExperiment assay assay<-
+#' @keywords internal
+create_se_full <- function(RCTD, results) {
+    se <- create_se_from_columns(RCTD, results, weights_col = "weights")
+    # Store weights redundantly for consistency with other modes.
+    assay(se, "weights_full") <- assay(se, "weights")
+    return(se)
+}
+
+#' Converts the results of \code{process_beads_batch} to a SummarizedExperiment
+#' 
+#' @param RCTD RCTD object
+#' @param results \code{process_beads_batch} results
+#'
+#' @importFrom SummarizedExperiment assay assay<- rowData rowData<-
+#' @keywords internal
+create_se_doublet <- function(RCTD, results) {
+    se <- create_se_from_columns(
+        RCTD,
+        results,
+        character_cols = c("spot_class", "first_type", "second_type"),
+        logical_cols = c(
+            "first_class", "second_class", "conv_all", "conv_doublet"
+        ),
+        numeric_cols = c("min_score", "singlet_score")
+    )
+
+    spot_class_levels <- c(
+        "reject", "singlet", "doublet_certain", "doublet_uncertain"
+    )
+    rowData(se)$spot_class <- factor(
+        rowData(se)$spot_class,
+        levels = spot_class_levels
+    )
+
+    weights <- assay(se, "weights")
+    # Generate doublet-mode weights from doublet_weights entry.
+    weights_doublet <- vapply(
+        seq_len(nrow(weights)),
+        function(pixel) {
+            weights_row <- numeric(ncol(weights))
+            names(weights_row) <- colnames(weights)
+
+            row_data <- rowData(se)[pixel, ]
+            doublet_weights <- results[[pixel]]$doublet_weights
+            weights_row[row_data$first_type] <- doublet_weights[1]
+            weights_row[row_data$second_type] <- doublet_weights[2]
+            weights_row
+        },
+        numeric(ncol(weights))
+    )
+    weights_doublet <- Matrix(t(weights_doublet), sparse = TRUE)
+
+    # Move the full weights and store the doublet-mode weights
+    assay(se, "weights_full") <- weights
+    assay(se, "weights", withDimnames = FALSE) <- weights_doublet
+    return(se)
+}
+
+#' Converts the results of \code{process_beads_multi} to a SummarizedExperiment
+#' 
+#' @param RCTD RCTD object
+#' @param results \code{process_beads_multi} results
+#'
+#' @importFrom SummarizedExperiment assay assay<- rowData
+#' @keywords internal
+create_se_multi <- function(RCTD, results) {
+    se <- create_se_from_columns(
+        RCTD,
+        results,
+        logical_cols = c("conv_all", "conv_sub"),
+        numeric_cols = c("min_score"),
+        list_cols = c("cell_type_list", "conf_list")
+    )
+
+    weights <- assay(se, "weights")
+    # Generate multi-mode weights from sub_weights entry.
+    weights_multi <- vapply(
+        seq_len(nrow(weights)),
+        function(pixel) {
+            weights_row <- numeric(ncol(weights))
+            names(weights_row) <- colnames(weights)
+
+            cell_types <- rowData(se)$cell_type_list[pixel]
+            sub_weights <- unlist(results[[pixel]]$sub_weights)
+            weights_row[unlist(cell_types)] <- sub_weights
+            weights_row
+        },
+        numeric(ncol(weights))
+    )
+    weights_multi <- Matrix(t(weights_multi), sparse = TRUE)
+
+    # Move the full weights and store the multi-mode weights
+    assay(se, "weights_full") <- weights
+    assay(se, "weights", withDimnames = FALSE) <- weights_multi
+    return(se)
+}
